361989c8134e907f901aabac9ace43d7cb74e3bf
bryonverdone <bryonverdone@gmail.com>
2022-08-01 17:56:56 -0400

    added coffee shop greeting

---
+++ ep3.py
@@ -0,0 +1,9 @@
+num1= 100
+num2=50
+userInput= int(input('please enter a number \n'))
+if(userInput >= num1):
+  print(f'This number is equal to or greater than {num1}')
+elif(userInput <= num2):
+  print(f'This number is less than or equal to {num2}')
+
+
