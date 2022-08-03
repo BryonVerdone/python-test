72269a6672989ee5395fa040e3f1e9faf41a6622
bryonverdone <bryonverdone@gmail.com>
2022-08-03 19:39:55 -0400

    added if else statement

---
+++ ep4.py
@@ -0,0 +1,13 @@
+# print('Hello and welcome to our coffee shop')
+
+# name= input('what is your name?')
+
+# if (name=='Ben'):
+#   print('your not welcome here evil Ben!')
+# else:
+#   print(f'hello {name} thank you for coming in today')
+
+if 2>3:
+  print('its true')
+else:
+  print('nope not true')
